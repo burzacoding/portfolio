5c2d8080f06fcebd46751d5a11053f3eb6875639
Cesar Pintos <pintos.cesar01@gmail.com>
2021-08-24 01:11:13 -0300

    Estilos para elemento Button

---
+++ src/styles/Index.ts
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const BaseLink = styled(Link)`
+  color: black;
+  text-decoration: none;
+  &:visited {
+    color: black;
+  }
+`;
